900c9efaf448bc2cb85e9e83e7d5f636e4080589
Francisco José <franciscojose2703@gmail.com>
2022-12-11 20:38:55 -0300

    atualizações na main

--- main.c
+++ main.c
@@ -2,65 +2,6 @@
 #include <stdlib.h>
 #include "heap.h"
 
-HEAP* HEAP_create(int n, COMP* compara) {
-	HEAP* heap = malloc(sizeof(HEAP));
-	heap->elems = malloc(n * sizeof(void*));
-	heap->N = n;
-	heap->P = 0;
-	heap->comparador = compara;
-
-	return heap;
-}
-
-void HEAP_add(HEAP* heap, void* newelem) {
-  if (heap->P < heap->N) {
-    heap->elems[heap->P] = newelem;
-	int pos = heap->P;
-
-	while (heap->comparador(heap->elems[pos], heap->elems[(pos - 1) / 2]) > 0) {
-		void* aux = heap->elems[pos];
-		heap->elems[pos] = heap->elems[(pos - 1) / 2];
-		heap->elems[(pos - 1) / 2] = aux;	
-		pos = (pos - 1) / 2;
-	}	
-
-	heap->P++;
-  }
-}
-
-void* HEAP_remove(HEAP* heap) {
-	void* min = heap->elems[0];
-
-	heap->elems[0] = heap->elems[heap->P - 1];
-	heap->elems[heap->P - 1] = min;
-	
-	heap->P--;
-
-	int pai = 0;
-	int filho = pai * 2 + 1;
-	
-	while (filho < heap->P) {
-		if (filho + 1 < heap->P) {
-			if (heap->comparador(heap->elems[filho], heap->elems[filho + 1]) < 0) {
-				filho += 1;
-			}
-		}
-
-		if (heap->comparador(heap->elems[pai], heap->elems[filho]) < 0) {
-			void* aux = heap->elems[pai];
-			heap->elems[pai] = heap->elems[filho];
-			heap->elems[filho] = aux;
-
-			pai = filho;
-			filho = pai * 2 + 1;
-		} else {
-			filho = heap->P + 1;
-		}
-	}
-
-	return min;
-}
-
 //Função de comparação
 int myCOMP(void* x, void* y){
 	int ptx = *((int*)x);
@@ -76,7 +17,7 @@ int myCOMP(void* x, void* y){
 //Print para a estrutura
 void print(void** vet, int P){
 	int i;
-	printf("VETOR DE %d TAMANHO:\n", P);
+	printf("VETOR DE %d POSIÇÕES:\n", P);
 	for(i = 0; i < 10; i++){
 		printf(" %d ", *((int*)vet[i]));
 	}
@@ -89,7 +30,7 @@ int main(){
    	int* x;
    	int i;
    	
-   	printf("INCLUINDO... \n");
+   	printf("INCLUINDO...\n");
 	for(i = 0; i < 10; i++){
 		x = malloc(sizeof(int));
 		*x = i*10;
@@ -98,7 +39,7 @@ int main(){
 	
 	print(vet->elems, vet->P);
   
-  	printf("REMOVENDO!");
+  	printf("REMOVENDO!\n");
   	HEAP_remove(vet);	
 	HEAP_remove(vet);	
 	HEAP_remove(vet);	
